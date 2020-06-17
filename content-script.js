c556cc3c7a43d7212ef86267f85b6cda670839bf
Wojciech Siewierski <wojciech@siewierski.eu>
2020-06-17 21:17:52 +0200

    Support the regular bundles too

--- content-script.js
+++ content-script.js
@@ -1,19 +1,27 @@
 "use strict";
 
-browser.runtime.onMessage.addListener(request => {
-  const games = request.games;
-
-  const choices = document.getElementsByClassName('content-choice');
-  for (let choice of choices) {
-    const titles = choice.getElementsByClassName('content-choice-title');
+function fadeOutBundle(owned_games, box_class, title_class) {
+  const bundled_games = document.getElementsByClassName(box_class);
+  for (let bundled_game of bundled_games) {
+    const titles = bundled_game.getElementsByClassName(title_class);
     for (let title of titles) {
       // Should be only one.
 
       const normalizedTitle = normalizeTitle(title.textContent);
-      if (games.has(normalizedTitle)) {
-        choice.style.opacity = 0.25;
+      if (owned_games.has(normalizedTitle)) {
+        bundled_game.style.opacity = 0.25;
       }
     }
+  }
+}
+
+browser.runtime.onMessage.addListener(request => {
+  const games = request.games;
+
+  if (window.location.pathname.match("^/subscription")) {
+    fadeOutBundle(games, 'content-choice', 'content-choice-title')
+  } else {
+    fadeOutBundle(games, 'game-boxes', 'front-page-art-image-text')
   }
 
   return Promise.resolve(null);
