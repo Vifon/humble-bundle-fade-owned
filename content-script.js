ebc32b5121bd46ecfd0cc025fc367106030f296a
Wojciech Siewierski <wojciech@siewierski.eu>
2020-09-13 22:47:48 +0200

    Refactor the selector logic to allow more flexibility in the future

--- content-script.js
+++ content-script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-function fadeOutBundle(owned_games, box_class, title_class) {
-  const bundled_games = document.getElementsByClassName(box_class);
+function fadeOutBundle(owned_games, box_find_func, title_find_func) {
+  const bundled_games = box_find_func(document);
   for (let bundled_game of bundled_games) {
-    const titles = bundled_game.getElementsByClassName(title_class);
+    const titles = title_find_func(bundled_game);
     for (let title of titles) {
       // Should be only one.
 
@@ -19,9 +19,17 @@ browser.runtime.onMessage.addListener(request => {
   const games = request.games;
 
   if (window.location.pathname.match("^/subscription")) {
-    fadeOutBundle(games, 'content-choice', 'content-choice-title')
+    fadeOutBundle(
+      games,
+      x => x.getElementsByClassName('content-choice'),
+      x => x.getElementsByClassName('content-choice-title')
+    )
   } else {
-    fadeOutBundle(games, 'game-boxes', 'front-page-art-image-text')
+    fadeOutBundle(
+      games,
+      x => x.getElementsByClassName('game-boxes'),
+      x => x.getElementsByClassName('front-page-art-image-text')
+    )
   }
 
   return Promise.resolve(null);
